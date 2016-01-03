909a9dbe4dc84828f44d9976a45444c39dae0263
Mihail Dzantiev <axel0726@gmail.com>
2016-01-03 18:03:36 +0300

    подвкладки

--- public/js/init.js
+++ public/js/init.js
@@ -36,6 +36,18 @@ var el =
 			$(instance).addClass('act');
 			var curTabIndex = $(instance).index();
 			$('.tabCont').removeClass('act').eq(curTabIndex).addClass('act');
+		},
+		// упарвление подвкладками
+		sub:
+		{
+			show:function(instance)
+			{
+				$(instance).siblings('.tab').removeClass('act');
+				$(instance).addClass('act');
+				var tabIndex = $(instance).data('tab');
+				$(instance).parents('.tabsLine').siblings('.subTabsCont').find('.subTabCont').removeClass('act');
+				$(instance).parents('.tabsLine').siblings('.subTabsCont').find('.subTabCont[data-tab="'+tabIndex+'"]').addClass('act');
+			}
 		}
 	},
 
