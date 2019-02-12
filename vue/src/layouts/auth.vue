e4404ea3d2542914dd481f672c438b6ee8d776c4
Kisiev <kisivaleri@gmail.com>
2019-02-12 13:46:29 +0300

    finish auth html

--- vue/src/layouts/auth.vue
+++ vue/src/layouts/auth.vue
@@ -1,19 +1,41 @@
 <template>
-	<div class="wrapper">
-		<div class="margin-bottom">
-			<input placeholder="Имя пользователя" v-model="user.login">
+	<div class="auth-content-wrapper">
+		<div class="auth-form" v-if="activeForm == 'login'">
+			<label class="auth-label">
+				<div class="auth-label-title">login</div>
+				<input type="text" placeholder="Enter your login" v-model="user.login" class="auth-form-input">
+			</label>
+			<label class="auth-label">
+				<div class="auth-label-title">Password</div>
+				<input type="password" placeholder="Enter your password" v-model="user.password" class="auth-form-input">
+			</label>
+			<div class="auth-bottom-btns">
+				<button class="auth-fill-btn" @click="authUser()">Log In</button>
+				<button class="auth-transpar-btn" @click="activeForm = 'forgot'">Forgot your password?</button>
+			</div>
 		</div>
-		<div class="margin-bottom">
-			<input placeholder="Пароль" v-model="user.password">
+		<div class="auth-form" v-if="activeForm == 'forgot'">
+			<label class="auth-label">
+				<div class="auth-label-title">login</div>
+				<input type="text" placeholder="Enter your login" class="auth-form-input">
+			</label>
+			<div class="auth-bottom-btns">
+				<button class="auth-fill-btn" @click="activeForm = 'sended'">Reset Password</button>
+				<button class="auth-transpar-btn" @click="activeForm = 'login'">Return to Log In page?</button>
+			</div>
 		</div>
-		<button class="primary" @click.stop.prevent="authUser">Отправить</button>
-		<div style="color: red" v-if="errors">{{errors}}</div>
+		<div class="auth-form auth-form-fix-heigth" v-if="activeForm == 'sended'">
+			New password was sended to your emial.
+		</div>
 	</div>
 </template>
 <script>
 export default
 {
 	name: 'Auth',
+	/**
+	 * Глобальные переменные странциы
+	 */
 	data()
 	{
 		return {
@@ -22,7 +44,8 @@ export default
 				login: '',
 				password: ''
 			},
-			errors: ''
+			errors: '',
+			activeForm: 'login'
 		}
 	},
 	methods:
@@ -47,3 +70,82 @@ export default
 	}
 }
 </script>
+<style lang="scss">
+	.auth-transpar-btn
+	{
+		font-size: 12px;
+		color: rgba(103, 115, 135, 0.7);
+		border: none;
+		cursor: pointer;
+	}
+	.auth-bottom-btns
+	{
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	.auth-fill-btn
+	{
+		background: rgba(25, 28, 33, 0.7);
+		border-radius: 2px;
+		color: #fff;
+		font-size: 12px;
+		padding: 7px 11px;
+		border: none;
+		margin-bottom: 11px;
+		cursor: pointer;
+	}
+	.auth-label
+	{
+		margin-bottom: 15px;
+		display: block;
+	}
+	.auth-label-title
+	{
+		color: #191C21;
+		margin-bottom: 7px;
+		font-size: 12px;
+	}
+	.auth-form-input
+	{
+		border: 1px solid rgba(103, 115, 135, 0.4);
+		border-radius: 2px;
+		width: 100%;
+		height: 30px;
+		font-size: 10px;
+		padding: 0 10px;
+		&::placeholder
+		{
+			color: rgba(103, 115, 135, 0.7);
+		}
+	}
+	.auth-content-wrapper
+	{
+		display: flex;
+		position: fixed;
+		z-index: 2;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		align-items: center;
+		justify-content: center;
+	}
+	.auth-form
+	{
+		width: 434px;
+		border: 1px solid rgba(103, 115, 135, 0.1);
+		border-radius: 2px;
+		padding: 30px 50px;
+		&.auth-form-fix-heigth
+		{
+			height: 181px;
+			align-items: center;
+			display: flex;
+			color: rgba(103, 115, 135, 0.7);
+			font-size: 12px;
+			justify-content: center;
+		}
+	}
+</style>
