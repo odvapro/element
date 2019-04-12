96dcc7fa898289125e5c7dbc40d480530e23d9c6
Serjlih97 <serjtema@yandex.ru>
2019-04-12 17:32:40 +0300

    add favicon

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -14,7 +14,20 @@
 	{
 		components: { Sprite, SetupDb, Content, Auth },
 		metaInfo:{
-			title: 'Element'
+			title: 'Element',
+			meta:
+			[
+				{ name: 'msapplication-TileColor', content: '#da532c' },
+				{ name: 'theme-color', content: '#ffffff' },
+			],
+			link:
+			[
+				{ rel:"apple-touch-icon", sizes:"180x180", href:"/apple-touch-icon.png" },
+				{ rel:"icon", type:"image/png", sizes:"32x32", href:"/favicon-32x32.png" },
+				{ rel:"icon", type:"image/png", sizes:"16x16", href:"/favicon-16x16.png" },
+				{ rel:"manifest", href:"/site.webmanifest" },
+				{ rel:"mask-icon", href:"/safari-pinned-tab.svg", color:"#5bbad5" }
+			]
 		},
 		computed:
 		{
