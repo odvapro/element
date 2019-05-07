73cf5b5e613bf8db220e37d69a1746bc4a53c830
Serjlih97 <serjtema@yandex.ru>
2019-05-07 13:50:07 +0300

    change router for REQUEST_URI

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,6 +1,11 @@
 <?php
 $router = new \Phalcon\Mvc\Router();
 
+// Use $_SERVER['REQUEST_URI']
+$router->setUriSource(
+    \Phalcon\Mvc\Router::URI_SOURCE_SERVER_REQUEST_URI
+);
+
 $router->add(
 	"/notfound/", [
 		"controller" => 'index',
