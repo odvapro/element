35400065408e20abab4fa273f39de0537042043e
sg-mon <s.g.simonyan@gmail.com>
2019-12-16 18:00:50 +0300

    start date include time

---
+++ vue/src/components/fields/em_date/Settings.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="">
+		<div class="popup__field">
+			<div class="popup__field-name">Include Time</div>
+			<div class="popup__field-input">
+				<Checkbox
+					:checked.sync="localSettings.includeTime"
+				></Checkbox>
+			</div>
+		</div>
+		<div class="popup__buttons">
+			<button @click="cancel" class="el-gbtn">Cancel</button>
+			<button @click="save" class="el-btn">Save settigns</button>
+		</div>
+	</div>
+</template>
+<script>
+	import Checkbox from '@/components/forms/Checkbox.vue';
+	export default
+	{
+		components: {Checkbox},
+		props: ['settings'],
+		data()
+		{
+			return {
+				localSettings:
+				{
+					includeTime: false,
+					required: true,
+				}
+			}
+		},
+		mounted()
+		{
+			if(typeof this.settings.includeTime != 'undefined')
+				this.localSettings.includeTime = this.settings.includeTime === "true";
+		},
+		methods:
+		{
+			save()
+			{
+				this.$emit('save', this.localSettings);
+			},
+			cancel()
+			{
+				this.$emit('cancel');
+			},
+		},
+	}
+</script>
