e6e39b822adc0cd401cac7e0ad4e011d305683f5
Михаил Дзантиев <axel0726@gmail.com>
2020-02-26 10:34:44 +0300

    editor

--- vue/src/components/fields/em_text/Field.vue
+++ vue/src/components/fields/em_text/Field.vue
@@ -1,22 +1,16 @@
 <template>
 	<div class="em-text" :class="{'em-text__table':(view != 'detail')}">
-		<template v-if="view=='detail'">
-			<div class="em-text__detail" @click="openEditor()">
-				{{ localValue }}
-			</div>
-		</template>
-		<template v-else>
-			<input
-				type="text"
-				class="el-inp-noborder"
-				@change="changeValue"
-				:value="localValue"
-				:placeholder="$t('empty')"
-			/>
-		</template>
+		<div class="em-text__txt" @click="openEditor()">
+			{{ previewText }}
+			<span class="el-empty" v-if="!previewText">{{ $t('empty') }}</span>
+		</div>
+		<Popup class="em-text__popup" :visible.sync="showEditor">
+			<div id="el-editor"></div>
+		</Popup>
 	</div>
 </template>
 <script>
+	import EditorJS from '@editorjs/editorjs';
 	export default
 	{
 		props: ['fieldValue','fieldSettings','mode', 'view'],
@@ -25,35 +19,53 @@
 		{
 			return {
 				localValue:this.fieldValue,
-				editorSettings:
+				showEditor:false,
+				editor:false,
+			}
+		},
+		computed:
+		{
+			previewText()
+			{
+				if(!this.localValue)
+					return '';
+				if(typeof this.localValue == 'object')
 				{
-					theme: 'bubble'
-				},
-				toolbarOptions:
-				[
-					[
-						'bold', 'italic','link', 'image',
-						{ 'list': 'ordered'}, { 'list': 'bullet' },
-						{ 'header': [1, 2, 3, 4, 5, 6, false] }
-					]
-				]
+					let blocksCount = (typeof this.localValue.blocks != 'undefined')?this.localValue.blocks.length:0;
+					let time =(typeof this.localValue.time != 'undefined')?this.localValue.time:0;
+					return `${blocksCount} blocks, last edit ${time}`;
+				}
+
+			}
+		},
+		watch:{
+			showEditor(show)
+			{
+				if(show == false)
+					this.editor.save().then((outputData) => {
+						this.localValue = outputData;
+						this.$emit('onChange', {
+							value     : this.localValue,
+							settings  : this.fieldSettings
+						});
+					})
 			}
 		},
 		methods:
 		{
-			/**
-			 * Send change current value
-			 */
-			changeValue(event)
-			{
-				this.$emit('onChange', {
-					value     :  event.target.value,
-					settings  : this.fieldSettings
-				});
-			},
 			openEditor()
 			{
-				alert('ok');
+				this.showEditor = true;
+
+				let data = {};
+				if(typeof this.localValue == 'string')
+					data = {blocks: [{"type": "paragraph", "data": {"text": this.localValue } } ] };
+				if(typeof this.localValue == 'object')
+					data = this.localValue;
+				this.editor = new EditorJS({
+					holderId : 'el-editor',
+					data     : data
+				});
 			}
 		}
 	}
@@ -75,12 +87,17 @@
 			margin-top: 0;
 		}
 	}
-	.em-text__detail
+	.em-text__txt
 	{
 		line-height: 17px;
 		font-size: 12px;
 		color: #677387;
 		padding:11px 20px 11px 0;
 		cursor: pointer;
+	}
+	.em-text__popup .popup-block
+	{
+		width:800px;
+		height:calc(100% - 144px);
 	}
 </style>
