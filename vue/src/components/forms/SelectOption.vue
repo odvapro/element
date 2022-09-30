11a9ca6244c7a8761da8f62f25e517eb93421176
Revaz333 <rezobol357@mail.ru>
2022-09-30 12:48:39 +0300

    fixed settings dropdown

--- vue/src/components/forms/SelectOption.vue
+++ vue/src/components/forms/SelectOption.vue
@@ -1,21 +1,3 @@
 <template>
-	<li @click="select"><slot></slot></li>
+	<li><slot></slot></li>
 </template>
-<script>
-	export default
-	{
-		name:"SelectOption",
-		methods:
-		{
-			/**
-			 * Choose option and close dropdown
-			 */
-			select: function(event)
-			{
-				var instance = event.target;
-				this.$parent.setContent(instance.innerHTML);
-				this.$parent.toggleDropdown();
-			}
-		}
-	}
-</script>
